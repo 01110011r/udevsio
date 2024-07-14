c748df7ae0412af77ec3d4c2de9d242697cf9ade
01110011r <s.rasulov@tuit.uz>
2024-07-14 22:14:42 +0500

    chore: Remove unused code ,   update dependencies and Navbar created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import Image from 'next/image'
+import React, { useState } from 'react'
+import mainlogo from "../../public/udevs-logo.png"
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { IoClose } from 'react-icons/io5';
+
+export default function Navbar() {
+
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const menuHandler = () => {
+    setMenu(!menu);
+    console.log(menu)
+  }
+
+  return (
+    <div className='fixed w-full right-0 left-0 border-b-2 z-10'>
+
+      <div className='container flex items-center justify-between px-2 py-6 relative overflow-hidden'>
+
+        <Image src={mainlogo} alt='udevs main logo' className='cursor-pointer w-24 h-10' />
+
+        <ul className='lg:flex items-center justify-between font-semibold hidden w-[70%]'>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Direction
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Command
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Services
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Tools
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Clients
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Portfolio
+            </a>
+          </li>
+          <li className='cursor-pointer'>
+            <a href="#">
+              Language
+            </a>
+          </li>
+          <li>
+            <button className='bg-blue-700 px-6 py-3 rounded-md text-white'>
+              <a href="#">
+                Contact
+              </a>
+            </button>
+          </li>
+        </ul>
+        <button className='inline-block lg:hidden' onClick={menuHandler}>
+          <RxHamburgerMenu />
+        </button>
+        <div className={`fixed bottom-0 top-0 flex flex-col bg-white transition-transform duration-500 ${!menu ? '-right-full' : 'left-0 right-0 lg:hidden'}`}>
+          <div className='flex items-center justify-between px-14 py-6 border-b-2'>
+            <Image src={mainlogo} alt='udevsio main logo' className='w-24 h-10' />
+            <button onClick={menuHandler}>
+              <IoClose className='w-8 h-8 text-blue-600' />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/*/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,6 +14,26 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    container:{
+      center: true,
+      screens: {
+        'xs': '475px',
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    }
   },
   plugins: [],
 };
