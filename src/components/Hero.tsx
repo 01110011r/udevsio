9bd02894576744c29e1014bb049f89386d297825
01110011r <s.rasulov@tuit.uz>
2024-07-16 18:22:52 +0500

    hero completed

---
+++ src/components/Hero.tsx
@@ -0,0 +1,31 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+import { ReactTyped } from 'react-typed'
+import HeroImg from '../../public/hero-img.png'
+
+export default function Hero() {
+  return (
+    <div className='pt-36 flex items-center justify-between lg:flex-row flex-col gap-y-10'>
+        <div className='flex flex-col gap-6 lg:items-start items-center lg:p-0 p-4'>
+            <h1 className='font-bold lg:text-6xl text-4xl'><span className='underline text-blue-600'>u</span>devs</h1>
+            <h3 className='font-semibold lg:text-2xl text-xl'>IT-Outsorcing Company</h3>
+            <ReactTyped
+            strings={[
+                'Optimization IT consulting infrastructure',
+                'User interface, User experience design',
+                'IT consulting',
+                'Development of mobile applications',
+                'Development and implementation ERP systems'
+            ]}
+            typeSpeed={70}
+            backSpeed={30}
+            loop
+            className='font-semibold lg:text-2xl text-xl text-blue-600'
+            />
+            <button className='text-white rounded-lg bg-blue-600 py-4'><a href="#" className='px-20 py-4'>Contact</a></button>
+        </div>
+        <Image src={HeroImg} alt='hero image'/>
+    </div>
+  )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,9 +16,9 @@ export default function Navbar() {
   }
 
   return (
-    <div className='fixed w-full right-0 left-0 border-b-2 z-10'>
+    <div className='fixed w-full right-0 left-0 border-b-2 z-10 bg-white'>
 
-      <div className='container flex items-center justify-between px-2 py-6 relative overflow-hidden'>
+      <div className='container flex items-center justify-between px-2 py-6 relative overflow-hidden transition'>
 
         <Image src={mainlogo} alt='udevs main logo' className='cursor-pointer w-24 h-10' />
 
@@ -67,15 +67,32 @@ export default function Navbar() {
           </li>
         </ul>
         <button className='inline-block lg:hidden' onClick={menuHandler}>
-          <RxHamburgerMenu />
+          <RxHamburgerMenu className='w-8 h-8 text-blue-600'/>
         </button>
-        <div className={`fixed bottom-0 top-0 flex flex-col bg-white transition-transform duration-500 ${!menu ? '-right-full' : 'left-0 right-0 lg:hidden'}`}>
+        <div className={`fixed bottom-0 top-0 flex flex-col justify-between bg-white duration-700 ${!menu ? '-right-full' : 'left-0 right-0 lg:hidden'}`}>
           <div className='flex items-center justify-between px-14 py-6 border-b-2'>
             <Image src={mainlogo} alt='udevsio main logo' className='w-24 h-10' />
             <button onClick={menuHandler}>
               <IoClose className='w-8 h-8 text-blue-600' />
             </button>
           </div>
+         <ul className='flex flex-col items-center font-semibold gap-4 text-3xl'>
+          <li>
+  <a href="#">Services</a>
+</li>
+<li>
+  <a href="#">Clients</a>
+</li>
+<li>
+  <a href="#">Command</a>
+</li>
+          </ul>
+         <div className='flex flex-col px-7 items-center gap-5'>
+        <button className='bg-blue-600 text-white px-6 py-2 rounded-lg w-full'><a href="#"> Contact </a></button>
+        <div>
+
+        </div>
+         </div>
         </div>
       </div>
     </div>
