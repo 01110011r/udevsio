dbd3e1947c171ab8a3c8954bae14a2a76f916340
01110011r <s.rasulov@tuit.uz>
2024-07-26 22:49:21 +0500

    fix:select

--- src/components/sections/Tools.tsx
+++ src/components/sections/Tools.tsx
@@ -9,29 +9,43 @@ export default function Tools() {
     <div className='py-14 bg-[#F4F7FF] w-full'>
         <div className=' container'>
         <h2 className=' lg:font-bold font-semibold text-5xl text-blue-600'>Tools</h2>
-        <form className='flex flex-col gap-5'>
-          <div>
+        <div className='grid lg:grid-cols-7 grid-cols-2 gap-2'>
             <label className='flex gap-2'>
-            <input type='radio' className='w-6'/>
-            <span className='text-3xl font-semibold'>Frontend</span>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>Frontend</span>
             </label>
 
             <label className='flex gap-2'>
-            <input type='radio' className='w-6'/>
-            <span className='text-3xl font-semibold'>Backend</span>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>Backend</span>
             </label>
 
             <label className='flex gap-2'>
-            <input type='radio' className='w-6'/>
-            <span className='text-3xl font-semibold'>DevOps</span>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>DevOps</span>
             </label>
 
             <label className='flex gap-2'>
-            <input type='radio' className='w-6'/>
-            <span className='text-3xl font-semibold'>Testing</span>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>Testing</span>
             </label>
-          </div>
-        </form>
+         
+            <label className='flex gap-2'>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>UX/UI</span>
+            </label>
+
+            <label className='flex gap-2'>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>Infrastructure</span>
+            </label>
+
+            <label className='flex gap-2'>
+            <input type='radio' className='w-6' name='tools_type'/>
+            <span className='text-2xl'>Mobile</span>
+            </label>
+ 
+        </div>
         </div>
     </div>
   )
